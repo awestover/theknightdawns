a8a072ae32458703413f13f69f9805c74b3f5a35
Alek Westover <alek.westover@gmail.com>
2019-06-08 16:01:28 -0400

    alek added movement

--- main.cpp
+++ main.cpp
@@ -1,28 +1,37 @@
+
 #include <SFML/Graphics.hpp>
 #include <iostream>
-// #include <stdio.h>
 using namespace std;
 
 int main()
 {
-	const sf::Vector2f viewSize(800, 800);
+	const int NUM_TILES = 25;
+	const int TILE_WIDTH = 8;
+	const sf::Vector2f screenDimensions(NUM_TILES*TILE_WIDTH, NUM_TILES*TILE_WIDTH);
+	const sf::Vector2f worldDimensions(NUM_TILES*TILE_WIDTH, NUM_TILES*TILE_WIDTH);
 
 	sf::RenderWindow window;
 	// window.create(sf::VideoMode(800, 800, 32), "RPG GAMMUUU!!", sf::Style::Fullscreen);
-	window.create(sf::VideoMode(viewSize.x, viewSize.y, 32), "RPG GAMMUUU!!");
+	// mainView.setViewport(sf::FloatRect()); // should scale
+	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y, 32), "RPG GAMMUUU!!");
 	window.setFramerateLimit(60);
 
-	sf::View mainView(sf::Vector2f(viewSize.x/2, viewSize.y/2), sf::Vector2f(viewSize.x, viewSize.y));
+	sf::View mainView(sf::Vector2f(screenDimensions.x/2, screenDimensions.y/2), sf::Vector2f(screenDimensions.x, screenDimensions.y));
 	mainView.setViewport(sf::FloatRect(0.0, 0.0, 1.0, 1.0));
 
-	// Load background img
-	sf::Texture texture;
-	texture.loadFromFile("data/tileMap.png");
-	sf::Sprite background;
-	background.setTexture(texture);
+	// Load images
+	sf::Texture bg_texture;
+	bg_texture.loadFromFile("data/tileMap.png");
+	sf::Sprite bg_sprite;
+	bg_sprite.setTexture(bg_texture);
 
-	// sf::Vector2f cameraPos = new sf::Vector2f(0, 0);
-	sf::Vector2f cameraPos();
+	sf::Texture dog_texture;
+	dog_texture.loadFromFile("data/dog.png");
+	sf::Sprite dog_sprite;
+	dog_sprite.setTexture(dog_texture);
+
+	sf::Vector2f cameraPos(0, 0);
+	sf::Vector2f userPos(0, 0);
 
     while (window.isOpen())
     {
@@ -35,22 +44,45 @@ int main()
 			}
 			else if (event.type == sf::Event::Resized)
 			{
-				float rectWidth = viewSize.x/event.size.width;
-				float rectHeight = viewSize.y/event.size.height;
+				float rectWidth = screenDimensions.x/event.size.width;
+				float rectHeight = screenDimensions.y/event.size.height;
 				mainView.setViewport(sf::FloatRect((1-rectWidth)/2,(1-rectHeight)/2,rectWidth,rectHeight));
 			}
 			else if (event.type == sf::Event::KeyPressed)
 			{
 				if (event.key.code == sf::Keyboard::Q)
 					window.close();
+				else if(event.key.code == sf::Keyboard::A)
+					userPos.x -= 10;
+				else if(event.key.code == sf::Keyboard::D)
+					userPos.x += 10;
+				else if(event.key.code == sf::Keyboard::W)
+					userPos.y -= 10;
+				else if(event.key.code == sf::Keyboard::S)
+					userPos.y += 10;
 			}
 		}
+
+		cameraPos.x = userPos.x + TILE_WIDTH/2;
+		if (cameraPos.x < screenDimensions.x/2)
+			cameraPos.x = screenDimensions.x/2;
+		if (cameraPos.x > worldDimensions.x - screenDimensions.x/2)
+			cameraPos.x = worldDimensions.x - screenDimensions.x/2;
+		cameraPos.y = userPos.y + TILE_WIDTH/2;
+		if (cameraPos.y < screenDimensions.y/2)
+			cameraPos.y = screenDimensions.y/2;
+		if (cameraPos.y > worldDimensions.y - screenDimensions.y/2)
+			cameraPos.y = worldDimensions.y - screenDimensions.y/2;
+		mainView.setCenter(cameraPos);
 
         window.clear(sf::Color(0, 0, 0, 1));
 		window.setView(mainView);
 
-		background.setTextureRect(sf::IntRect(100, 100, viewSize.x, viewSize.y));
-        window.draw(background);
+		bg_sprite.setTextureRect(sf::IntRect(0, 0, screenDimensions.x, screenDimensions.y));
+        window.draw(bg_sprite);
+		dog_sprite.setPosition(userPos);
+		window.draw(dog_sprite);
+
         window.display();
     }
 
